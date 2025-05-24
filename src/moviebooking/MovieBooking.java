3da8fea10677b75b0c57fd4e550925f577542ab1
Dpatil1604 <patildipali2004@gmail.com>
2025-05-24 06:58:04 -0400

    Add files via upload
    1

---
+++ src/moviebooking/MovieBooking.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package moviebooking;
+
+/**
+ *
+ * @author deepa
+ */
+public class MovieBooking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new login1().setVisible(true);
+        }
+    });
+}
+
+    
+}
